dbdfd38d3dd0a0a0a8aaebdb1b269b64dc9bdfa6
Aleksandar Hristov <64472282+AleksandarHristovGithub@users.noreply.github.com>
2021-08-06 08:46:03 +0300

    git add

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  // baseUrl: 'http://localhost:3200'
-  baseUrl: 'http://localhost:3200'
+
 };
 
 /*
